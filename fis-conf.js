36aef0bb1f0437ad40e7e65d5cf63b848d3bcb5d
Larry Xu <hilarryxu@gmail.com>
2018-11-20 18:42:39 +0800

    Add Vue

--- fis-conf.js
+++ fis-conf.js
@@ -1,23 +1,52 @@
+fis.set('project.files', ['src/**']);
 
-// npm install [-g] fis3-hook-commonjs
-fis.hook('commonjs');
-
-fis.match('/comp/**/*.js', {
-    isMod: true, // 设置 comp 下都是一些组件，组件建议都是匿名方式 define
-    release: '/static/$0'
+fis.hook('commonjs', {
+    baseUrl: 'src',
+    paths: {
+        'vue': '/node_modules/vue/dist/vue.js'
+    }
 });
 
-fis.match('::package', {
-    // npm install [-g] fis3-postpackager-loader
-    // 分析 __RESOURCE_MAP__ 结构，来解决资源加载问题
-    postpackager: fis.plugin('loader', {
-        resourceType: 'commonJs',
-        useInlineMap: true // 资源映射表内嵌
-    })
+// :dev
+fis.match('/src/(**)', {
+    release: '/dist/$1',
+    url: '/dist/$1'
 })
+.match('/src/(**).js', {
+    id: '$1',
+    isMod: true
+})
+.match('/src/plugins/**.js', {
+  isMod: false
+})
+.match('/(node_modules/**)', {
+  release: '/dist/$1',
+  url: '$1'
+})
+.match('/(node_modules/**.js)', {
+  isMod: true,
+  useSameNameRequire: false
+})
+.match('/node_modules/**.map', {
+  release: false
+})
+.match('::package', {
+    postpackager: fis.plugin('loader', {
+        resourceType: 'mod',
+        useInlineMap: true
+    })
+});
 
-// fis3 release prod 产品发布，进行合并
-fis.media('prod')
-    .match('*.js', {
-        packTo: '/static/aio.js'
-    });
+// :prod
+fis
+.media('prod')
+.match('*.js', {
+    packTo: '/dist/aio.js'
+})
+.match('::package', {
+    postpackager: fis.plugin('loader', {
+        resourceType: 'mod',
+        resourcemapWhitespace: 0,
+        useInlineMap: true
+    })
+});
